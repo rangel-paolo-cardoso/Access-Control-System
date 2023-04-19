f3a2e3af762907d6c667f391181d176d99bee406
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-04-18 21:51:38 -0300

    Initialized repository.

---
+++ src/program/AccessControlSystem.java
@@ -0,0 +1,11 @@
+package program;
+public class AccessControlSystem {
+
+    /**
+     * Main Method.
+     * The @param args is used to pass information into the program.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
